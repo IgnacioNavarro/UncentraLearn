ce2b66ff8f539bdc44298991d9c0184efffa74e5
IgnacioNavarro <inavarroblazquez@gmail.com>
2024-01-28 00:03:13 +0100

    more header

--- frontend/uncentralearn/components/header.tsx
+++ frontend/uncentralearn/components/header.tsx
@@ -5,39 +5,41 @@ import LogoCompleto from "./logoCompleto";
 
 export default function Header() {
     return (
-        <header className="flex items-center justify-between p-4">
-        <div className="flex items-center justify-between">
-            <Link href="/">
-                <LogoCompleto/>
-            </Link>
-        </div>
-        <div className="flex items-center justify-between">
-            <Link href="/courses">
-                <span className="text-xl">Cursos</span>
-            </Link>
-            <Link href="/sponsors">
-                <span className="text-xl">Patrocinadores</span>
-            </Link>
-            <Link href="/jobs">
-                <span className="text-xl">Empleo</span>
-            </Link>
+        <header className="flex items-center bg-rose-50">
+            <div className="flex items-center justify-between px-10 color-white">
+                <Link href="/">
+                    <LogoCompleto/>
+                </Link>
+            </div>
+            <div className="flex items-start p-4 space-x-4">
+                <Link href="/courses">
+                    <span className="text-xl">Cursos</span>
+                </Link>
+                <Link href="/sponsors">
+                    <span className="text-xl">Patrocinadores</span>
+                </Link>
+                <Link href="/jobs">
+                    <span className="text-xl">Empleo</span>
+                </Link>
+            </div>
+            <div className="flex items-center justify-end space-x-4">
 
-        </div>
-        <div className="flex items-center justify-between">
+                <input type="text" placeholder="Buscar" className="border-2 border-gray-300 rounded-md p-1" />
 
-            <input type="text" placeholder="Buscar" className="border-2 border-gray-300 rounded-md p-1"/>
-            
 
-            <button className="bg-white hover:bg-blue-700 text- font-bold py-2 px-4 rounded">
+                <button className="bg-white hover:bg-rose-50 hover:text-white text-rose-50 py-2 px-4 rounded">
 
-            <Link href="/login">
-                <span className="text-xl font-bold">Iniciar Sesión</span>
-            </Link>
-            </button>
-            <Link href="/register">
-                <span className="text-xl font-bold">Registrarse</span>
-            </Link>
-        </div>
+                    <Link href="/login">
+                        <span className="text-xl">Iniciar Sesión</span>
+                    </Link>
+                </button>
+
+                <button className="bg-white hover:bg-rose-50 hover:text-white text-rose-50 py-2 px-4 rounded">
+                    <Link href="/register">
+                        <span className="text-xl">Registrarse</span>
+                    </Link>
+                </button>
+            </div>
         </header>
     );
-    }
+}
